e00c4e473f640101fe85c8f71dfdc33452d09ffc
Gram <master_fess@mail.ru>
2018-07-10 16:47:22 +0300

    can stop benchmark at any moment

--- benchmark.py
+++ benchmark.py
@@ -3,33 +3,37 @@ import numpy
 
 pure = []
 patched = []
-for _ in range(10):
-    pure.append(timeit(
-        setup=(
-            "from requests import Session\n"
-            "sess = Session()\n"
-        ),
-        stmt=(
-            "sess.get('https://httpbin.org/get')\n"
-            "sess.post('https://httpbin.org/post')\n"
-        ),
-        number=1,
-    ))
-
-    patched.append(timeit(
-        setup=(
-            "from requests_connection import Session, Connection\n"
-            "conn = Connection.https('httpbin.org')\n"
-            "sess = Session(conn)\n"
+for _ in range(200):
+    try:
+        pure.append(timeit(
+            setup=(
+                "from requests import Session\n"
+                "sess = Session()\n"
             ),
-        stmt=(
-            "sess.get('https://httpbin.org/get')\n"
-            "sess.post('https://httpbin.org/post')\n"
-        ),
-        number=1,
-    ))
+            stmt=(
+                "sess.get('https://httpbin.org/get')\n"
+                "sess.post('https://httpbin.org/post')\n"
+            ),
+            number=1,
+        ))
+
+        patched.append(timeit(
+            setup=(
+                "from requests_connection import Session, Connection\n"
+                "conn = Connection.https('httpbin.org')\n"
+                "sess = Session(conn)\n"
+                ),
+            stmt=(
+                "sess.get('https://httpbin.org/get')\n"
+                "sess.post('https://httpbin.org/post')\n"
+            ),
+            number=1,
+        ))
+    except KeyboardInterrupt:
+        break
 
 
+count = min([len(pure), len(patched)])
 pure = min(pure), sum(pure) / len(pure), max(pure)
 patched = min(patched), sum(patched) / len(patched), max(patched)
 
@@ -42,3 +46,5 @@ print('Patched:  {:.03} ± {:.4}'.format(
     numpy.mean(patched),
     numpy.std(patched),
 ))
+
+print('Runs: ', count)
